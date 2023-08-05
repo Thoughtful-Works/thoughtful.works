b051d8f822539bb1a8606bd04f507227df088fdc
Amit Kumar <amit@thoughtful.works>
2023-08-05 14:14:43 -0400

    initialize new next.js site

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Amit Kumar – Engineering Leader, Software Architect, Entrepreneur",
+  description: "Portfolio website",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
