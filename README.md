8c86b852735aefbd501f4c6b1326e784299e6288
Amit Kumar <amit@thoughtful.works>
2023-08-06 00:58:15 -0400

    wiring tiles into json and setting up react-move animation library

--- README.md
+++ README.md
@@ -19,9 +19,11 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 
 This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
 
+## Deployed on Vercel
+
 ## References
 
-- [react-hexgrid](https://github.com/Hellenic/react-hexgrid)
+- [react-hexgrid](https://github.com/Hellenic/react-hexgrid) for hexagon tiles
+- [react-move](https://github.com/sghall/react-move) for animations
 - [Hex map geometry and algorithms](https://www.redblobgames.com/grids/hexagons/)
-
-## Deployed on Vercel
+- [Google Fonts Pair Finder](https://10015.io/tools/google-fonts-pair-finder)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,11 +8,32 @@ import {
   Pattern,
   Path,
   Hex,
+  GridGenerator,
 } from "react-hexgrid";
-
+import { Animate, NodeGroup } from "react-move";
 import Image from "next/image";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
+
+const baseHexGrid = GridGenerator.spiral(new Hex(0, 0, 0), 5); // 91 cells
+console.log(baseHexGrid);
+
+const offeringsData = [
+  {
+    title: "Web Software Architect",
+  },
+  {
+    title: "Engineering Leader",
+  },
+  {
+    title: "Entrepreneur",
+  },
+  {
+    title: "Children's Book Author",
+  },
+];
+
+const experienceData = [];
 
 export default function Home() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -46,24 +67,44 @@ export default function Home() {
             origin={{ x: 0, y: 0 }}
             flat={false}
           >
-            <Hexagon
-              q={0}
-              r={0}
-              s={0}
-              className={styles.tile}
-              onPointerOver={(e) => {
-                e.currentTarget.style.fill = "red";
-              }}
-              onPointerOut={(e) => {
-                e.currentTarget.style.fill = "";
+            <NodeGroup
+              data={offeringsData}
+              keyAccessor={(item) => item.title}
+              start={(item, index) => {
+                return {
+                  q: baseHexGrid[index].q,
+                  r: baseHexGrid[index].r,
+                  s: baseHexGrid[index].s,
+                };
               }}
             >
-              <Text>Web Software Architect</Text>
-            </Hexagon>
-            <Hexagon q={0} r={-1} s={0} className={styles.tile} />
-            <Hexagon q={0} r={1} s={0} className={styles.tile} />
-            <Hexagon q={-1} r={0} s={0} className={styles.tile} />
-            <Hexagon q={1} r={0} s={0} className={styles.tile} />
+              {(nodes) => {
+                return (
+                  <g>
+                    {nodes.map(({ key, data, state: { q, r, s } }) => {
+                      return (
+                        // Hexagon q, r, s props accept decimals, which means we should be able to interpolate them with an animation library
+                        <Hexagon
+                          key={key}
+                          q={q}
+                          r={r}
+                          s={s}
+                          className={styles.tile}
+                          onPointerOver={(e) => {
+                            e.currentTarget.style.fill = "red";
+                          }}
+                          onPointerOut={(e) => {
+                            e.currentTarget.style.fill = "";
+                          }}
+                        >
+                          <Text>{data.title}</Text>
+                        </Hexagon>
+                      );
+                    })}
+                  </g>
+                );
+              }}
+            </NodeGroup>
           </Layout>
         </HexGrid>
       </div>
